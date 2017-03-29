07c8ea7f7f72b32b1d7f6e0a8e459d630c97215a
Pilar Figueroa <pilar.fc@outlook.com>
2017-03-29 12:06:45 -0600

    Ya me crea Listas pero aún no tiene todo el formto

--- trello.js
+++ trello.js
@@ -1,35 +1,39 @@
 
 function crearPendiente () {
-var elementosListaCrearPendientesSemana = document.getElementById("usuarioCrearPendientesSemana").value;
+  var elementosListaCrearPendientesSemana = document.getElementById("usuarioCrearPendientesSemana").value;
 
-var lista = document.createElement("li");
-var textoLista = document.createTextNode(elementosListaCrearPendientesSemana);
-lista.appendChild(textoLista);
-document.getElementById("pendientesSemana").appendChild(lista);
+  var lista = document.createElement("li");
+  var textoLista = document.createTextNode(elementosListaCrearPendientesSemana);
+    lista.appendChild(textoLista);
+    document.getElementById("pendientesSemana").appendChild(lista);
 
-var botonBorrar = document.createElement("button");
+  var botonBorrar = document.createElement("button");
 }
 
 function crearPendientesOtraSemana () {
-var elementosListaCrearPendientesOtraSemana = document.getElementById("infoPendientesOtraSemana").value;
-
-var lista = document.createElement("li");
-var textoLista = document.createTextNode(elementosListaCrearPendientesOtraSemana);
-lista.appendChild(textoLista);
-document.getElementById("pendientesOtraSemana").appendChild(lista);
-}
-
-function borrarPendiente() {
-  var borrar = document.getElementById("pendientesSemana");
-  borrar.removeChild(borrar.childNodes[0]);
-}
-
-
-function crearLista () {
-var crearLista = document.getElementById("crearNuevaLista").value;
+  var elementosListaCrearPendientesOtraSemana = document.getElementById("infoPendientesOtraSemana").value;
 
   var lista = document.createElement("li");
+  var textoLista = document.createTextNode(elementosListaCrearPendientesOtraSemana);
+    lista.appendChild(textoLista);
+    document.getElementById("pendientesOtraSemana").appendChild(lista);
+}
+
+function borrarPendienteUno() {
+  var borrar = document.getElementById("pendientesSemana");
+    borrar.removeChild(borrar.childNodes[0]);
+}
+
+function borrarPendienteDos() {
+  var borrar = document.getElementById("pendientesOtraSemana");
+    borrar.removeChild(borrar.childNodes[0]);
+}
+
+function crearLista () {
+  var crearLista = document.getElementById("crearNuevaLista").value;
+
+  var lista = document.createElement("section");
   var textoLista = document.createTextNode(crearLista);
-  lista.appendChild(textoLista);
-  document.getElementById("contenedor").appendChild(lista);
+    lista.appendChild(textoLista);
+    document.getElementById("contenedor").appendChild(lista);
 }
